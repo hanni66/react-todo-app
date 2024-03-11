6788982e3fe82755cdf5f504aaf65c900db87b33
hanni66 <haeunkim0807@gmail.com>
2024-03-12 00:48:38 +0900

    todo앱 최적화 하기

--- src/App.js
+++ src/App.js
@@ -1,99 +1,51 @@
-import React, {Component} from "react";
 import "./App.css";
+import { useState } from "react";
+import Lists from "./components/Lists";
+import Form from "./components/Form";
 
-export default class App extends Component {
+const initialTodoData = localStorage.getItem("todoData")
+  ? JSON.parse(localStorage.getItem("todoData"))
+  : [];
 
-  state = {
-    todoData : [],
-    value: "",
-  }
+function App() {
+  const [todoData, setTodoData] = useState(initialTodoData);
+  const [value, setValue] = useState("");
 
-  btnStyle = {
-    color: "#fff",
-    border: "none", 
-    padding: "5px 9px",
-    borderRadius: "50%",
-    cursor: "pointer",
-    float: "right"
-  }
-
-  getStyle = (completed) => {
-    return {
-      padding: "10px",
-      borderBottom: "1px #ccc dotted",
-      textDecoration: completed ? "line-through" : "none",
-    }
-  }
-
-  handleClick = (id) => {
-    let newTodoData = this.state.todoData.filter((data) => data.id !== id);
-    console.log('newTodoData', newTodoData);
-    this.setState({ todoData: newTodoData });
-  };
-
-  handleChange = (e) => {
-    this.setState({value: e.target.value});
-  }
-
-  handleSubmit = (e) => {
-    // form 안에 input을 전송할 때 페이지 리로드 되는 것을 막아줌
+  const handleSubmit = (e) => {
+    // form 안에 input을 전송할 때 페이지 리로드 되는 걸 막아줌
     e.preventDefault();
 
-    // 새로운 todo data
+    // 새로운 할 일 데이터
     let newTodo = {
       id: Date.now(),
-      title: this.state.value,
+      title: value,
       completed: false,
     };
 
-    this.setState({ todoData: [...this.state.todoData, newTodo], value: ""});
-  }
+    setTodoData((prev) => [...prev, newTodo]);
+    localStorage.setItem("todoData", JSON.stringify([...todoData, newTodo]));
 
-  handleCompleteChange = (id) => {
-    let newTodoData = this.state.todoData.map(data => {
-      if (data.id === id) {
-        data.completed = !data.completed;
-      }
-      return data;
-    });
-    this.setState({ todoData: newTodoData });
-  }
+    setValue("");
+  };
 
-  // 원래 있던 할 일에 새로운 할 일 추가
-  render() {
-    return(
-      <div className="container">
-        <div className="todoBlock">
-          <div className="title">
-            <h1>할 일 목록</h1>
-          </div>
+  const handleRemoveClick = () => {
+    setTodoData([]);
+    localStorage.setItem("todoData", JSON.stringify([]));
+  };
 
-          {this.state.todoData.map((data) => (
-            <div style={this.getStyle(data.completed)} key={data.id}>
-              <input type="checkbox" defaultChecked={false} onChange={() => this.handleCompleteChange(data.id)}/>
-              {" "}{data.title}
-              <button style={this.btnStyle} onClick={() => this.handleClick(data.id)}>x</button>
-            </div>
-          ))}
-
-          <form style={{ display: 'flex'}} onSubmit={this.handleSubmit}>
-            <input 
-              type="text" 
-              name="value" 
-              style={{flex: '10', padding: '5px'}} 
-              placeholder="해야 할 일을 입력하세요." 
-              value={this.state.value}
-              onChange={this.handleChange}
-            />
-            <input 
-              type="submit" 
-              value="입력" 
-              className="btn" 
-              style={{flex: 1}} 
-            />
-          </form>
+  return (
+    <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
+      <div className="w-full p-6 m-4 bg-white rounded shadow md:w-3/4 md:max-w-lg lg:w-3/4 lg:max-w-lg">
+        <div className="flex justify-between mb-3">
+          <h1>할 일 목록</h1>
+          <button onClick={handleRemoveClick}>Delete All</button>
         </div>
+        <Lists todoData={todoData} setTodoData={setTodoData} />
+
+        <Form handleSubmit={handleSubmit} value={value} setValue={setValue} />
       </div>
-    )
-  }
+    </div>
+  );
 }
+
+export default App;
